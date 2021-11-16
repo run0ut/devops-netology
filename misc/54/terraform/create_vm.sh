5b7cf0f7916bf8a893a5c0187cb06c9b57832341
Sergey Shadurskiy <s.shadurskiy@gmail.com>
2021-11-16 23:21:38 +0500

    Забыл удалить файлы, генерируемые при apply

--- misc/54/terraform/create_vm.sh
+++ misc/54/terraform/create_vm.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash 
 
+rm -f terraform.tfstate.backup
+rm -f terraform.tfstate
+
 ../erase.sh terraform
 export CENTOS_7_BASE=$(yc compute images get --name centos-7-base --format json | jq -r .id)
 terraform apply -auto-approve
