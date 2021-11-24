4ec0afaf5ac636ca697d46fb209930447df4b2e4
Sergey Shadurskiy <s.shadurskiy@gmail.com>
2021-11-24 17:01:34 +0500

    У переменных из env должен быть префикс TF_VAR https://www.terraform.io/docs/language/values/variables.html#environment-variables

--- misc/55/terraform/create_vm.sh
+++ misc/55/terraform/create_vm.sh
@@ -2,8 +2,14 @@
 
 rm -f terraform.tfstate.backup
 rm -f terraform.tfstate
-
 ../erase.sh terraform
-export CENTOS_7_BASE=$(yc compute images get --name centos-7-base --format json | jq -r .id) 
+
+export TF_VAR_yandex_cloud_id=$(yc config get cloud-id) 
+export TF_VAR_yandex_folder_id=$(yc config get folder-id) 
+export TF_VAR_centos_7_base=$(yc compute images get --name centos-7-base --format json | jq -r .id) 
+
 terraform apply -auto-approve
-unset CENTOS_7_BASE
+
+unset TF_VAR_yandex_cloud_id \
+      TF_VAR_yandex_folder_id \
+      TF_VAR_centos_7_base
