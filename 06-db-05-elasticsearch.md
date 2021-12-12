db634e27a08101167dced11ad9856b5f24cfc779
Sergey Shadurskiy <s.shadurskiy@gmail.com>
2021-12-12 18:05:56 +0500

    65: 3 done

--- 06-db-05-elasticsearch.md
+++ 06-db-05-elasticsearch.md
@@ -203,3 +203,68 @@ yellow open   ind-2            5gq69worTjeYcZp_dZiLnA   2   1          0        
 > - возможно вам понадобится доработать `elasticsearch.yml` в части директивы `path.repo` и перезапустить `elasticsearch`
 
 </details>
+
+### **Приведите в ответе** запрос API и результат вызова API для создания репозитория.
+
+`Запрос`
+```bash
+curl -X PUT "localhost:9200/_snapshot/netology_backup?pretty" -H 'Content-Type: application/json' -d'
+{
+  "type": "fs",
+  "settings": {
+    "location": "/var/lib/elasticsearch/snapshots",
+    "compress": true
+  }
+}'
+```
+`Ответ`
+```json
+{
+  "acknowledged" : true
+}
+```
+### Создайте индекс `test` с 0 реплик и 1 шардом и **приведите в ответе** список индексов.
+
+```tsv
+health status index            uuid                   pri rep docs.count docs.deleted store.size pri.store.size
+green  open   .geoip_databases xCjT_XLWT96qt1-Kp4xXgQ   1   0         42            0     41.1mb         41.1mb
+green  open   test             2ocwHrSiQtqAIvuX04yt7g   1   0          0            0       226b           226b
+```
+
+### **Приведите в ответе** список файлов в директории со `snapshot`ами.
+
+```bash
+$ docker exec elastic3 ls -l /var/lib/elasticsearch/snapshots/
+total 28
+-rw-r--r-- 1 elasticsearch elasticsearch 1434 Dec 12 12:21 index-0
+-rw-r--r-- 1 elasticsearch elasticsearch    8 Dec 12 12:21 index.latest
+drwxr-xr-x 6 elasticsearch elasticsearch 4096 Dec 12 12:21 indices
+-rw-r--r-- 1 elasticsearch elasticsearch 9737 Dec 12 12:21 meta-fzn6GSxgSX-JC9wHwIWhNA.dat
+-rw-r--r-- 1 elasticsearch elasticsearch  458 Dec 12 12:21 snap-fzn6GSxgSX-JC9wHwIWhNA.dat
+```
+
+### Удалите индекс `test` и создайте индекс `test-2`. **Приведите в ответе** список индексов.
+
+```tsv
+health status index            uuid                   pri rep docs.count docs.deleted store.size pri.store.size
+green  open   .geoip_databases xCjT_XLWT96qt1-Kp4xXgQ   1   0         42            0     41.1mb         41.1mb
+green  open   test-2           2NVi48R_QqW73lxawMykRA   1   0          0            0       226b           226b
+```
+
+### **Приведите в ответе** запрос к API восстановления и итоговый список индексов.
+
+`запрос к API восстановления` + запросы из [документации](https://www.elastic.co/guide/en/elasticsearch/reference/current/snapshots-restore-snapshot.html) на выключение/включение фич и удалени индексов копировал 1 в 1
+```bash
+curl -X POST "localhost:9200/_snapshot/netology_backup/my_snapshot_2021.12.12/_restore?pretty" -H 'Content-Type: application/json' -d'
+{
+  "indices": "*",
+  "include_global_state": false
+}
+'
+```
+`итоговый список индексов`
+```tsv
+health status index            uuid                   pri rep docs.count docs.deleted store.size pri.store.size
+green  open   .geoip_databases 4zMXz3RMR-SY48JnHNJWlQ   1   0         42            0     41.1mb         41.1mb
+green  open   test             bf7x24BMS-iplwoPN0PzWQ   1   0          0            0       226b           226b
+```
--- misc/65/indexes.sh
+++ misc/65/indexes.sh
@@ -4,6 +4,9 @@ indexes="ind-1 	0 	1
 ind-2 	1 	2
 ind-3 	2 	4"
 
+indexes31="test  	0 	1"
+indexes32="test-2  	0 	1"
+
 exec_cmd(){
     c="$@"
     echo $c 
@@ -19,9 +22,20 @@ $indexes
 EnfOfList
 }
 
-status(){
-    exec_cmd "curl http://172.17.0.2:9200/_cat/indices?v=true"
-    exec_cmd "curl -ss http://172.17.0.2:9200/_cluster/health?pretty"
+create31(){
+    while read name repl shards; do
+        exec_cmd "curl -ss -XPUT http://localhost:9200/${name} -H 'Content-Type: application/json' -d '{\"settings\":{\"index\":{\"number_of_replicas\":${repl},\"number_of_shards\":${shards}}}}'"
+    done << EnfOfList
+$indexes31
+EnfOfList
+}
+
+create32(){
+    while read name repl shards; do
+        exec_cmd "curl -ss -XPUT http://localhost:9200/${name} -H 'Content-Type: application/json' -d '{\"settings\":{\"index\":{\"number_of_replicas\":${repl},\"number_of_shards\":${shards}}}}'"
+    done << EnfOfList
+$indexes32
+EnfOfList
 }
 
 delete(){
@@ -30,6 +44,67 @@ delete(){
     done << EnfOfList
 $indexes
 EnfOfList
+}
+
+delete31(){
+    while read name repl shards; do
+        exec_cmd "curl -ss -XDELETE http://localhost:9200/${name}"
+    done << EnfOfList
+$indexes31
+EnfOfList
+}
+
+delete32(){
+    while read name repl shards; do
+        exec_cmd "curl -ss -XDELETE http://localhost:9200/${name}"
+    done << EnfOfList
+$indexes32
+EnfOfList
+}
+
+backup_configure(){
+    curl -X PUT "localhost:9200/_snapshot/netology_backup?pretty" -H 'Content-Type: application/json' -d'
+{
+  "type": "fs",
+  "settings": {
+    "location": "/var/lib/elasticsearch/snapshots",
+    "compress": true
+  }
+}'
+}
+
+backup_create(){
+    curl -X PUT "localhost:9200/_snapshot/netology_backup/%3Cmy_snapshot_%7Bnow%2Fd%7D%3E?pretty&wait_for_completion=true"
+}
+
+backup_restore(){
+    # Остановить индексирование 
+    curl -X PUT "localhost:9200/_cluster/settings?pretty" -H 'Content-Type: application/json' -d'{"persistent": {"ingest.geoip.downloader.enabled": false}}'
+    curl -X POST "localhost:9200/_ilm/stop?pretty"
+    curl -X POST "localhost:9200/_ml/set_upgrade_mode?enabled=true&pretty"
+    curl -X PUT "localhost:9200/_cluster/settings?pretty" -H 'Content-Type: application/json' -d'{"persistent": {"xpack.monitoring.collection.enabled": false}}'
+    curl -X POST "localhost:9200/_watcher/_stop?pretty"
+    # Отключить безопасное удаление
+    curl -X PUT "localhost:9200/_cluster/settings?pretty" -H 'Content-Type: application/json' -d'{"persistent": {"action.destructive_requires_name": false}}'
+    # Удалить все индексы
+    curl -X DELETE "localhost:9200/_data_stream/*?expand_wildcards=all&pretty"
+    curl -X DELETE "localhost:9200/*?expand_wildcards=all&pretty"
+    # Получить имя последнего бекапа
+    snapshot_name=$(curl -ss -X GET "localhost:9200/_snapshot/netology_backup/*?pretty&size=1&sort=name&order=desc" | jq -r .snapshots[0].snapshot)
+    # Восстановить 
+    curl -X POST "localhost:9200/_snapshot/netology_backup/${snapshot_name}/_restore?pretty" -H 'Content-Type: application/json' -d'{"indices": "*","include_global_state": false}'
+    # Включить индексирование
+    curl -X PUT "localhost:9200/_cluster/settings?pretty" -H 'Content-Type: application/json' -d'{"persistent": {"ingest.geoip.downloader.enabled": true}}'
+    curl -X POST "localhost:9200/_ilm/start?pretty"
+    curl -X POST "localhost:9200/_ml/set_upgrade_mode?enabled=false&pretty"
+    curl -X PUT "localhost:9200/_cluster/settings?pretty" -H 'Content-Type: application/json' -d'{"persistent": {"xpack.monitoring.collection.enabled": true}}'
+    curl -X POST "localhost:9200/_watcher/_start?pretty"
+}
+
+status(){
+    exec_cmd "curl http://localhost:9200/_cat/indices?v=true"
+    exec_cmd "curl -ss http://localhost:9200/_cluster/health?pretty"
+    exec_cmd "curl -ss http://localhost:9200/_snapshot?pretty"
 }
 
 case ${1:-not_args} in
