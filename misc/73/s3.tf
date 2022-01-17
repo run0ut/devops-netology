8620bf7dea50cde8565c0939ddd857abb6d16832
Sergey Shadurskiy <s.shadurskiy@gmail.com>
2022-01-17 23:53:28 +0500

    72: s3, добавил dynamodb + fmt

--- misc/73/s3.tf
+++ misc/73/s3.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "netology-73"
-    key    = "devops-netology/virt-11/73/terraform.tfstate"
-    region = "eu-north-1"
+    bucket         = "netology-73"
+    key            = "devops-netology/virt-11/73/terraform.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "netology-tf-state-locking"
   }
 }
