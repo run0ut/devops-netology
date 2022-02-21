3184baa6aecdc61c57de8d35b8ec09a6b2197411
Sergey Shadurskiy <s.shadurskiy@gmail.com>
2022-02-22 01:21:54 +0500

    добавлено памяти нодам, т.к. вылетал oom killer

--- 03-mnt-homeworks/08-ansible-03-yandex/playbook/terraform/yandex.tf
+++ 03-mnt-homeworks/08-ansible-03-yandex/playbook/terraform/yandex.tf
@@ -27,8 +27,8 @@ resource "yandex_compute_instance" "elk" {
 
   resources {
     cores         = 2
-    memory        = 2
-    core_fraction = 20
+    memory        = 4
+    core_fraction = 100
   }
 
   boot_disk {
@@ -57,8 +57,8 @@ resource "yandex_compute_instance" "fbeat" {
 
   resources {
     cores         = 2
-    memory        = 2
-    core_fraction = 20
+    memory        = 4
+    core_fraction = 100
   }
 
   boot_disk {
