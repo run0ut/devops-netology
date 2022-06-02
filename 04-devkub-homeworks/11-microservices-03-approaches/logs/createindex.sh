84146657ace63d3d874c6309f9f307d8af1b40c8
Sergey Shadurskiy <s.shadurskiy@gmail.com>
2022-06-02 20:15:23 +0500

    11.3: автосоздание индекс паттерна kibana

---
+++ 04-devkub-homeworks/11-microservices-03-approaches/logs/createindex.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# create_index=$(curl -v -X POST http://elastic:qwerty123456@kibana:5601/api/index_patterns/index_pattern \
+#     -H "kbn-xsrf: true" \
+#     -H "Content-Type: application/json" \
+#     -d'
+#     {
+#         "override": false,
+#         "refresh_fields": true,
+#         "index_pattern": {
+#             "title":"vector*",
+#             "timeFieldName":"timestamp"
+#         }
+#     }')
+# echo "create_index="$create_index
+ret_code=22
+http_code=$(curl --silent --output $(mktemp) -X POST http://elastic:qwerty123456@kibana:5601/api/index_patterns/index_pattern \
+    -H "kbn-xsrf: true" \
+    -H "Content-Type: application/json" \
+    -d'
+    {
+        "override": false,
+        "refresh_fields": true,
+        "index_pattern": {
+            "title":"vector*",
+            "timeFieldName":"timestamp"
+        }
+    }' \
+    --write-out "%{http_code}")
+if [ "$http_code" == "000" ]; then
+    ret_code=22
+elif [ "$http_code" != "200" ]; then
+    ret_code=${http_code}
+else
+    ret_code=22
+fi
+echo $ret_code
+exit "$ret_code"
