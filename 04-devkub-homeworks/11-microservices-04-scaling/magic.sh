6d39867d15e5732f84e7858a98a08080ea892366
Sergey Shadurskiy <s.shadurskiy@gmail.com>
2022-06-08 00:25:01 +0500

    11.4: playbook для создания хостов и установки redis, in progress

--- 04-devkub-homeworks/11-microservices-04-scaling/magic.sh
+++ 04-devkub-homeworks/11-microservices-04-scaling/magic.sh
@@ -9,3 +9,6 @@ is_role_installed=$(ansible-galaxy collection list --format yaml | grep -c -w ne
 if [[ "$is_role_installed" == "0" ]]; then
     ansible-galaxy install -r requirements.yml
 fi
+
+ansible-playbook -i yc_inventory.yml playbook.yml
+# ansible-playbook -i yc_inventory.yml playbook.yml --start-at-task "Download redis.rpm file"
