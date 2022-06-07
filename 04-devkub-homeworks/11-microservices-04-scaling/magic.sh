ff388cf4a8b671b04c2f6b609c8cd838f0855d10
Sergey Shadurskiy <s.shadurskiy@gmail.com>
2022-06-07 22:40:30 +0500

    11.4: установка роли Ansible

---
+++ 04-devkub-homeworks/11-microservices-04-scaling/magic.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -x
+# set -euo pipefail
+
+echo "Hi, Netology 11.4! Let's make some devops stuff!"
+
+is_role_installed=$(ansible-galaxy collection list --format yaml | grep -c -w netology86.yandex_cloud_elk)
+if [[ "$is_role_installed" == "0" ]]; then
+    ansible-galaxy install -r requirements.yml
+fi
