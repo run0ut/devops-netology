89b39f45b27787c2035a2a12cb38b4307a88125a
Sergey Shadurskiy <s.shadurskiy@gmail.com>
2022-06-26 23:03:21 +0500

    13.2: 2 done, основная часть done

---
+++ 04-devkub-homeworks/13-kubernetes-config-01-objects/create-switch-namespace.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# простой скрипт, в основном чтобы не забыть команду переключения неймспейса у текущего контекста
+declare NS=${1:-no_arg}
+case $NS in
+    "stage"|"prod")
+        kubectl create namespace $NS
+        kubectl config set-context --current --namespace=$NS
+        ;;
+    *)
+        echo "Введите stage или prod"
+        ;;
+esac
