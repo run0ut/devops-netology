5775a07f236f3de5c58f528d305cf625f6f8e917
Sergey Shadurskiy <s.shadurskiy@gmail.com>
2022-10-16 20:09:51 +0500

    Диплом: ключи к бакету всё-таки пришлось отправить в репозиторий

--- 06-devops-diplom-yandexcloud/02/01-yandex/provider.tf
+++ 06-devops-diplom-yandexcloud/02/01-yandex/provider.tf
@@ -10,8 +10,8 @@ terraform {
     bucket     = "dimploma-bucket"
     region     = "ru-central1"
     key        = "terraform.tfstate"
-    access_key = "<идентификатор статического ключа>"
-    secret_key = "<секретный ключ>"
+    access_key = "YCAJE9DZKigNrEv5obI_OeP-g"
+    secret_key = "YCPp4hLAM2RicUjTKOPfLWgOy-10YsHACA1WNj5_"
 
     skip_region_validation      = true
     skip_credentials_validation = true
@@ -19,7 +19,7 @@ terraform {
 }
 
 provider "yandex" {
-  service_account_key_file = "key.json"
+  service_account_key_file = "/home/atlantis/key.json"
   cloud_id = var.yandex_cloud_id
   folder_id = var.yandex_folder_id
   zone = var.yandex_compute_default_zone
