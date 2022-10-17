f9463425d2ac2a8833be3c1c0e6dcd4c5705b09e
Sergey Shadurskiy <s.shadurskiy@gmail.com>
2022-10-17 23:15:20 +0500

    Диплом: перенос разных секретов в variables.tf; автогенерация 01-yandex/provider.tf с ключами бакета

---
+++ 06-devops-diplom-yandexcloud/02/00-bucket/30-render_template.tf
@@ -0,0 +1,33 @@
+################################################################################
+# Рендер ../01-yandex/provider.tf с ключами к бакету
+# Чтобы не писать руками
+
+# -------------------------------------------------
+# Файл для импорта логина и пароля к аккаунту Докера
+# в Jenkins Credentials
+data "template_file" "provider" {
+  template = file("${path.module}/templates/provider.tpl")
+
+  vars = {
+    access_key = yandex_iam_service_account_static_access_key.bucket-operator.access_key
+    secret_key = yandex_iam_service_account_static_access_key.bucket-operator.secret_key
+  }
+
+  depends_on = [
+    yandex_iam_service_account_static_access_key.bucket-operator
+  ]
+}
+
+# -------------------------------------------------
+# Сохранение рендера креденшелов в файл
+resource "null_resource" "provider" {
+  count = (terraform.workspace == "prod") ? 1 : 0
+
+  provisioner "local-exec" {
+    command = "${format("cat <<\"EOF\" > \"%s\"\n%s\nEOF", "../01-yandex/provider.tf", data.template_file.provider.rendered)}"
+  }
+
+  triggers = {
+    template = data.template_file.provider.rendered
+  }
+}
