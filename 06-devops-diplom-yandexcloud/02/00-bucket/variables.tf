5666d5d65dc967f880151f572bafafcda0bf2d39
Sergey Shadurskiy <s.shadurskiy@gmail.com>
2022-10-18 23:04:48 +0500

    Диплом: переменные в одном месте

--- 06-devops-diplom-yandexcloud/02/00-bucket/variables.tf
+++ 06-devops-diplom-yandexcloud/02/00-bucket/variables.tf
@@ -1,3 +1,5 @@
+################################################################################
+# Yandex IDs
 variable "yandex_cloud_id" {
   default = ""
 }
@@ -7,5 +9,31 @@ variable "yandex_folder_id" {
 }
 
 variable "yandex_compute_default_zone" {
+  default = ""
+}
+
+################################################################################
+# DockerHub
+variable "dockerhub_login" {
+  default = ""
+}
+
+variable "dockerhub_password" {
+  default   = ""
+  sensitive = true
+}
+
+################################################################################
+# GitHub
+variable "github_personal_access_token" {
+  default   = ""
+  sensitive = true
+}
+
+variable "github_webhook_secret" {
+  default = "diplomasecret"
+}
+
+variable "github_login" {
   default = ""
 }
